1cd88cc3a8e81131a012d21fe8c1719a29c18ac8
SAFAA <safaamghar12@gmail.com>
2025-05-17 03:58:15 +0100

    Delete assets/js directory

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const cols = document.querySelectorAll('#myRow .col');
-cols.forEach((col, index) => {
-  if (index % 2 === 0) {
-    col.classList.add('color1');
-  } else {
-    col.classList.add('color2');
-  }
-});
